6ffd4e3c6f159106924917ecc2f7f3c060b740a8
Дмитрий Кудрин <volyyyk@inbox.ru>
2024-03-23 22:34:50 +0300

    Add if name main

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
     run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
@@ -97,6 +97,3 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
